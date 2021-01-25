e87c59a37d1bd22f5cc111190b233c43094bad74
ShidaJingTetra <sjing@tetrascience.com>
2021-01-24 19:24:23 -0500

    Implemented basic functionality. Needs code-cleanup

--- src/google_utils.py
+++ src/google_utils.py
@@ -7,39 +7,68 @@ from document import Document
 
 import json
 
+import pprint
+
+from googleapiclient.discovery import build
+
 
 
 def get_results(query: List[str]) -> List[Document]:
     """
     Search the query using Google Engine and return list of document objects
     """
-    pass
+    ret = []
 
-print("I'm here")
+    if len(query) == 0:
+      return []
+    
+    concat_query = ' '.join(query)
 
-import pprint
-
-from googleapiclient.discovery import build
-
-
-def main():
-  # Build a service object for interacting with the API. Visit
-  # the Google APIs Console <http://code.google.com/apis/console>
-  # to get an API key for your own application.
-  service = build("customsearch", "v1",
+    service = build("customsearch", "v1",
             developerKey="AIzaSyAXqKtB1mQUHZmrFmMW_EhNK3JAyWRAK9o")
 
-  res = service.cse().list(
-      q='Linear Algebra Lectures',
-      cx='2c6600e3a2ae8bcd0',
+    res = service.cse().list(
+      q = concat_query,
+      cx = '2c6600e3a2ae8bcd0',
     ).execute()
 
-  json_object = json.loads(res['items'][0])
+    list_of_results = res['items']
 
-  json_formatted_str = json.dumps(json_object, indent=2)
+    for item in list_of_results:
+      title = item["title"]
+      url = item["link"]
+      desc = item["snippet"]
 
-  print(json_formatted_str)
-#   pprint.pprint(res)
+      doc = Document(title, url, desc)
 
-if __name__ == '__main__':
-  main()
+      ret.append(doc)
+
+
+    for item in ret:
+      item.print_doc()
+      
+    return ret
+
+    # json_object = json.loads(json.dumps(res['items'], indent = 4))
+
+    # json_formatted_str = json.dumps(json_object, indent=2)
+
+    # print(json_formatted_str)
+
+
+
+
+
+
+
+# def main():
+#   # Build a service object for interacting with the API. Visit
+#   # the Google APIs Console <http://code.google.com/apis/console>
+#   # to get an API key for your own application.
+
+#   que = ["image", "processing"]
+
+#   get_results(que)
+
+# if __name__ == '__main__':
+#   main()
